3dc7be319060d0c922f0534d14c21ef90bec1760
PODER <PODER@10.255.15.191>
2017-05-26 09:53:05 -0500

    Nueva version

--- src/java/com/app/servlets/ServletProyecto.java
+++ src/java/com/app/servlets/ServletProyecto.java
@@ -1,16 +1,28 @@
-
 package com.app.servlets;
 
+import com.app.controlador.ControladorProyecto;
+import com.app.modelo.conexion.ConexionBD;
+import com.app.modelo.dto.ProyectoUsuarioDTO;
+import com.app.modelo.vo.ProyectoVO;
+import com.app.modelo.vo.UsuarioVO;
+import com.app.utils.exceptions.ProyectoException;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.naming.NamingException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
-@WebServlet(name = "ServletProyecto", urlPatterns = {"/ServletProyecto"})
+@WebServlet(name = "ServletProyecto", urlPatterns = {"/Proyecto"})
 public class ServletProyecto extends HttpServlet {
 
     /**
@@ -21,16 +33,52 @@ public class ServletProyecto extends HttpServlet {
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
+     * @throws java.text.ParseException
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+            throws ServletException, IOException, ParseException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
+
+            int idFicha = Integer.parseInt(request.getParameter("idFicha"));
+            int idUsuario = Integer.parseInt(request.getParameter("idUsuario"));
+            String nombreProyecto = request.getParameter("nombre");
+            String descripcion = request.getParameter("descripcion");
+            String fechaInicio = request.getParameter("fechainicio");
+            SimpleDateFormat formato = new SimpleDateFormat("dd/mm/yyyy");
+            Date fecha = new Date(formato.parse(fechaInicio).getTime());
+            String fechaFin = request.getParameter("fechafin");
+            SimpleDateFormat formato1 = new SimpleDateFormat("dd/mm/yyyy");
+            Date fecha1 = new Date(formato1.parse(fechaFin).getTime());
+
+            ProyectoVO vo = new ProyectoVO();
+            ProyectoUsuarioDTO vt = new ProyectoUsuarioDTO();
+            vo.setIdFichas(idFicha);
+            vt.setUv(new UsuarioVO());
+            vt.getUv().setIdUsuario(idUsuario);
+            vo.setNombreProyecto(nombreProyecto);
+            vo.setDescripcion(descripcion);
+            vo.setFechaInicio(fecha);
+            vo.setFechaFin(fecha1);
+
+            try {
+                Connection cnn = ConexionBD.getConexionBD();
+                ControladorProyecto control = new ControladorProyecto(cnn);
+                ProyectoVO idProyecto = control.crearProyecto(vo);
+                vt.setPv(idProyecto);
+                control.usuarioProyecto(vt);
+                ConexionBD.desconectarBD(cnn);
+            } catch (ProyectoException prex) {
+                System.out.println(prex.getErrorAplicacion().getCodigo() + "> -- <" + prex.getErrorAplicacion().getMensaje());
+            } catch (NamingException | SQLException ex) {
+                System.out.println("Error BD" + ex.getMessage());
+
+            }
+
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet ServletProyecto</title>");            
+            out.println("<title>Servlet ServletProyecto</title>");
             out.println("</head>");
             out.println("<body>");
             out.println("<h1>Servlet ServletProyecto at " + request.getContextPath() + "</h1>");
@@ -51,7 +99,11 @@ public class ServletProyecto extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            processRequest(request, response);
+        } catch (ParseException ex) {
+            Logger.getLogger(ServletProyecto.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
@@ -65,7 +117,11 @@ public class ServletProyecto extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            processRequest(request, response);
+        } catch (ParseException ex) {
+            Logger.getLogger(ServletProyecto.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
